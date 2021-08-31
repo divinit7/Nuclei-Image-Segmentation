bb6d66e6afa0e40cb4a5858c24436020656de81b
Divyanshu Chauhan <chauhandivyanshu14@gmail.com>
2021-08-31 15:49:02 +0530

    adding v1

---
+++ prepare_dataset.py
@@ -0,0 +1,31 @@
+"""
+Script to combine multiple masks available for each sample into a single image
+"""
+from contextlib import ExitStack
+from multiprocessing import Pool
+from pathlib import Path
+
+from PIL import Image
+from tqdm import tqdm
+
+
+def combine_masks(mask_root_dir):
+    mask_output_fn = mask_root_dir/'mask.png'
+    if mask_output_fn.exists():
+        return
+    mask_fn_iter = mask_root_dir.glob('masks/*.png')
+    img = Image.open(next(mask_fn_iter))
+    for fn in mask_fn_iter:
+        mask = Image.open(fn)
+        img.paste(mask, (0, 0), mask)
+    img.save(mask_output_fn)
+
+# Combine masks into one
+samples_dirs = list(d for d in Path('./data/cells').iterdir() if d.is_dir())
+
+if __name__ == '__main__':
+    with ExitStack() as stack:
+        pool = stack.enter_context(Pool())
+        pbar = stack.enter_context(tqdm(total=len(samples_dirs)))
+        for _ in  tqdm(pool.imap_unordered(combine_masks, samples_dirs)):
+            pbar.update()
